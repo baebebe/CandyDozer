b55e18751ffeeb8581b52e7294a42d571c9cf1db
배진섭 <poxy1535@danuon.co.kr>
2017-06-19 18:09:47 +0900

    유니티 완료 프로젝트 -유니티5 로 만드는2D/3D 스마트폰 게임 개발 Chapter4

---
+++ Assets/Scripts/CandyDestroyer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandyDestroyer : MonoBehaviour {
+
+	public CandyHolder candyHolder;
+	public int reward;
+	public GameObject effectPrefab;
+	public Vector3 effectRotation;
+	int score = 0;
+	public int scoreNum;
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "Candy") {
+			candyHolder.AddCandy (reward);
+			if(reward != 0)
+				score += scoreNum;
+			Destroy (other.gameObject);
+
+			if (effectPrefab != null) {
+				Instantiate (effectPrefab, other.transform.position, Quaternion.Euler (effectRotation));
+			}
+		}
+	}
+
+	void OnGUI(){
+		if (reward != 0) {
+			GUI.color = Color.black;
+			string label = "Score: " + score;
+			GUI.Label (new Rect (0, 30, 100, 30), label);
+		}
+	}
+}
